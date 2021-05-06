6824194f591b86fc8f8e99b3a7a6f57b777e61e1
RobertoImpacta <70179811+RobertoImpacta@users.noreply.github.com>
2021-05-06 00:09:17 -0300

    deletar o cliente

--- PROJETO_CODIGO/app.py
+++ PROJETO_CODIGO/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, g
 from admin import admin
-from controllers import *
 
 
 app = Flask(__name__)
@@ -15,5 +14,7 @@ app.register_blueprint(
 )
 
 
+from controllers import *
+
 if __name__ == "__main__":
-    app.run(debug=True, host='127.0.0.1', port='5000')
+    app.run(debug=True, host='0.0.0.0', port='5000')
